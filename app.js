1f15561ea09ec73b06b5236ac97e5f0c897d46a7
KristenLingwood <kristen.lingwood@gmail.com>
2018-10-31 11:47:25 -0700

    correction for cors issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const gistRoutes = require('./routes/gists');
 const APIError = require('./APIError');
@@ -9,13 +10,22 @@ const APIError = require('./APIError');
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use(cors());
+
+// var allowCrossDomain = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+
+//   app.use(allowCrossDomain); // plumbing it in as middleware
+
 app.use('/gists', gistRoutes);
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  return next(err);
-});
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   return next(err);
+// });
 
 app.use((error, req, res, next) => {
   // format built-in errors

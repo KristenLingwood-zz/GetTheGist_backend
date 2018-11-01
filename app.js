4b0d51f42e7e1c9b1c85a8ca271c1f335f97211a
KristenLingwood <kristen.lingwood@gmail.com>
2018-11-01 10:48:16 -0700

    update app.js and gistpostschema to allow for no extension input

--- app.js
+++ app.js
@@ -12,20 +12,9 @@ app.use(morgan('dev'));
 
 app.use(cors());
 
-// var allowCrossDomain = function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
-
-//   app.use(allowCrossDomain); // plumbing it in as middleware
 
 app.use('/gists', gistRoutes);
 
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   return next(err);
-// });
 
 app.use((error, req, res, next) => {
   // format built-in errors
